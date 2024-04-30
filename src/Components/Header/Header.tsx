fa7d23371e6389e5625d6c9e45e0a69c86590b24
Victtor777 <victtor.eng.software@gmail.com>
2024-04-30 17:12:58 -0300

    Menu Bases

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,36 +1,71 @@
-import { BiGridAlt, BiMoon } from "react-icons/bi";
+import { BiGridAlt, BiX } from "react-icons/bi";
 import styles from "./Header.module.css";
 import logo from "../../assets/logo.png";
+import navLights from "../../assets/nav-light.png";
+import { useState } from "react";
 
 export function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
+  function closeMenu() {
+    setIsMenuOpen(false);
+  }
+
   return (
-    <header className={``}>
-      <nav>
-        <a href="">
-          <img src={logo} alt="" width={22} />
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <a className={styles.nav_logo} href="#">
+          <img className={styles.nav_logo_img} src={logo} alt="" width={22} />
           Christimas
         </a>
-
-        <div>
+        <div
+          className={`${styles.nav_menu} ${isMenuOpen ? styles.show_menu : ""}`}
+        >
           <ul>
             <li>
-              <a href="">Home</a>
+              <a onClick={closeMenu} href="#home">
+                Home
+              </a>
             </li>
             <li>
-              <a href="">Celebrate</a>
+              <a onClick={closeMenu} href="#celebrate">
+                Celebrate
+              </a>
             </li>
             <li>
-              <a href="">Gifts</a>
+              <a onClick={closeMenu} href="#gifts">
+                Gifts
+              </a>
             </li>
             <li>
-              <a href="">New</a>
+              <a onClick={closeMenu} href="#new">
+                New
+              </a>
             </li>
           </ul>
-          <img src="" alt="" />
+
+          <div
+            onClick={toggleMenu}
+            className={`${styles.nav_close} ${
+              isMenuOpen ? styles.show_menu : ""
+            }`}
+          >
+            <BiX />
+          </div>
+
+          <img
+            className={styles.nav_img_menu}
+            src={navLights}
+            width={100}
+            alt=""
+          />
         </div>
 
-        <div>
-          <BiMoon size={20} />
+        <div onClick={toggleMenu} className={styles.nav_toggle}>
           <BiGridAlt size={20} />
         </div>
       </nav>
